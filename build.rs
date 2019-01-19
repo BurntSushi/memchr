d01b35e9f903c888d23c628c7631cc6912284869
Andrew Gallant <jamslam@gmail.com>
2019-01-19 08:12:24 -0500

    deps: remove version_check
    memchr is a core crate and should ideally have as few dependencies as
    possible.

--- build.rs
+++ build.rs
@@ -1,18 +1,23 @@
-extern crate version_check;
-
 use std::env;
-
-use version_check::is_min_version;
+use std::ffi::OsString;
+use std::process::Command;
 
 fn main() {
-    enable_simd_optimizations();
+    let version = match Version::read() {
+        Ok(version) => version,
+        Err(err) => {
+            eprintln!("failed to parse `rustc --version`: {}", err);
+            return;
+        }
+    };
+    enable_simd_optimizations(version);
 }
 
-fn enable_simd_optimizations() {
-    if is_env_set("CARGO_CFG_MEMCHR_DISABLE_AUTO_SIMD") {
+fn enable_simd_optimizations(version: Version) {
+    if env::var_os("CARGO_CFG_MEMCHR_DISABLE_AUTO_SIMD").is_some() {
         return;
     }
-    if !is_min_version("1.27.0").map(|(yes, _)| yes).unwrap_or(false) {
+    if version < (Version { major: 1, minor: 27, patch: 0 }) {
         return;
     }
 
@@ -22,6 +27,62 @@ fn enable_simd_optimizations() {
     println!("cargo:rustc-cfg=memchr_runtime_avx");
 }
 
-fn is_env_set(name: &str) -> bool {
-    env::var(name).is_ok()
+#[derive(Clone, Copy, Debug, Eq, PartialEq, PartialOrd, Ord)]
+struct Version {
+    major: u32,
+    minor: u32,
+    patch: u32,
+}
+
+impl Version {
+    fn read() -> Result<Version, String> {
+        let rustc = env::var_os("RUSTC").unwrap_or(OsString::from("rustc"));
+        let output = Command::new(&rustc)
+            .arg("--version")
+            .output()
+            .unwrap()
+            .stdout;
+        Version::parse(&String::from_utf8(output).unwrap())
+    }
+
+    fn parse(mut s: &str) -> Result<Version, String> {
+        if !s.starts_with("rustc ") {
+            return Err(format!("unrecognized version string: {}", s));
+        }
+        s = &s["rustc ".len()..];
+
+        let parts: Vec<&str> = s.split(".").collect();
+        if parts.len() < 3 {
+            return Err(format!("not enough version parts: {:?}", parts));
+        }
+
+        let mut num = String::new();
+        for c in parts[0].chars() {
+            if !c.is_digit(10) {
+                break;
+            }
+            num.push(c);
+        }
+        let major = num.parse::<u32>().map_err(|e| e.to_string())?;
+
+        num.clear();
+        for c in parts[1].chars() {
+            if !c.is_digit(10) {
+                break;
+            }
+            num.push(c);
+        }
+        let minor = num.parse::<u32>().map_err(|e| e.to_string())?;
+
+        num.clear();
+        for c in parts[2].chars() {
+            if !c.is_digit(10) {
+                break;
+            }
+            num.push(c);
+        }
+        let patch = num.parse::<u32>().map_err(|e| e.to_string())?;
+
+        Ok(Version { major, minor, patch })
+    }
 }
