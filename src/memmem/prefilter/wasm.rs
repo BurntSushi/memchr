cfa2fda7d2b6cbe6c8a161421d4665181dc5e3b5
Alex Crichton <alex@alexcrichton.com>
2021-12-22 12:59:57 -0600

    impl: add wasm simd support
    This commit adds simd acceleration support to the `memmem` module. This
    is added with the freshly-stabilized support from rust-lang/rust#86204.
    This mostly just cribs off the generic simd support for 128-bit types
    built for sse, copying bits and pieces of code here and there. Some
    refactoring happened internally to help reduce duplication where
    possible.
    
    I ran some initial benchmarks with the `memmem/krate/*` regex and a
    hacked up single-threaded version of criterion. Some [initial
    comparisons][compare] using Wasmtime as a runtime do indeed show a lot
    of improvements, but there are indeed some slowdowns as well.
    
    [compare]: https://gist.github.com/alexcrichton/6a72e682e7b6d505ade605359fbe3f2d
    
    PR #84

--- src/memmem/prefilter/wasm.rs
+++ src/memmem/prefilter/wasm.rs
@@ -1,4 +1,4 @@
-use core::arch::x86_64::__m128i;
+use core::arch::wasm32::v128;
 
 use crate::memmem::{
     prefilter::{PrefilterFnTy, PrefilterState},
@@ -8,26 +8,23 @@ use crate::memmem::{
 // Check that the functions below satisfy the Prefilter function type.
 const _: PrefilterFnTy = find;
 
-/// An SSE2 accelerated candidate finder for single-substring search.
-///
-/// # Safety
-///
-/// Callers must ensure that the sse2 CPU feature is enabled in the current
-/// environment. This feature should be enabled in all x86_64 targets.
-#[target_feature(enable = "sse2")]
-pub(crate) unsafe fn find(
+/// A `v128`-accelerated candidate finder for single-substring search.
+#[target_feature(enable = "simd128")]
+pub(crate) fn find(
     prestate: &mut PrefilterState,
     ninfo: &NeedleInfo,
     haystack: &[u8],
     needle: &[u8],
 ) -> Option<usize> {
-    super::super::genericsimd::find::<__m128i>(
-        prestate,
-        ninfo,
-        haystack,
-        needle,
-        super::super::simple_memchr_fallback,
-    )
+    unsafe {
+        super::genericsimd::find::<v128>(
+            prestate,
+            ninfo,
+            haystack,
+            needle,
+            super::simple_memchr_fallback,
+        )
+    }
 }
 
 #[cfg(all(test, feature = "std"))]
